8dc1e669b413548f5850700372c36ec45492f282
Dominik <dominik.dolezall@gmail.com>
2021-01-10 18:51:12 +0100

    Hello World

---
+++ Devslopes/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Devslopes
+//
+//  Created by Dominik Doležal on 10/01/2021.
+//  Copyright © 2021 Do-Simeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Ahoj já se mám dobře")
+    }
+
+
+}
+
